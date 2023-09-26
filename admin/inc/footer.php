88bec3b720e87dc665149956582728e9997cae06
Huyh Huyhu <2509roblox@gmail.com>
2023-09-26 10:14:28 +0700

    first commit

---
+++ admin/inc/footer.php
@@ -0,0 +1,74 @@
+</div>
+</div>
+<!-- page-wrapper End-->
+
+<!-- Modal Start -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog  modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-body">
+                <h5 class="modal-title" id="staticBackdropLabel">Logging Out</h5>
+                <p>Are you sure you want to log out?</p>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <div class="button-box">
+                    <button type="button" class="btn btn--no" data-bs-dismiss="modal">No</button>
+                    <button type="button" class="btn  btn--yes btn-primary">Yes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal End -->
+
+<!-- latest js -->
+<script src="assets/js/jquery-3.6.0.min.js"></script>
+
+<!-- Bootstrap js -->
+<script src="assets/js/bootstrap/bootstrap.bundle.min.js"></script>
+
+<!-- feather icon js -->
+<script src="assets/js/icons/feather-icon/feather.min.js"></script>
+<script src="assets/js/icons/feather-icon/feather-icon.js"></script>
+
+<!-- scrollbar simplebar js -->
+<script src="assets/js/scrollbar/simplebar.js"></script>
+<script src="assets/js/scrollbar/custom.js"></script>
+
+<!-- Sidebar jquery -->
+<script src="assets/js/config.js"></script>
+
+<!-- tooltip init js -->
+<script src="assets/js/tooltip-init.js"></script>
+
+<!-- Plugins JS -->
+<script src="assets/js/sidebar-menu.js"></script>
+<script src="assets/js/notify/bootstrap-notify.min.js"></script>
+<script src="assets/js/notify/index.js"></script>
+
+<!-- Apexchar js -->
+<script src="assets/js/chart/apex-chart/apex-chart1.js"></script>
+<script src="assets/js/chart/apex-chart/moment.min.js"></script>
+<script src="assets/js/chart/apex-chart/apex-chart.js"></script>
+<script src="assets/js/chart/apex-chart/stock-prices.js"></script>
+<script src="assets/js/chart/apex-chart/chart-custom1.js"></script>
+
+
+<!-- slick slider js -->
+<script src="assets/js/slick.min.js"></script>
+<script src="assets/js/custom-slick.js"></script>
+
+<!-- customizer js -->
+<script src="assets/js/customizer.js"></script>
+
+<!-- ratio js -->
+<script src="assets/js/ratio.js"></script>
+
+<!-- sidebar effect -->
+<script src="assets/js/sidebareffect.js"></script>
+
+<!-- Theme js -->
+<script src="assets/js/script.js"></script>
+</body>
+
+</html>
---
+++ admin/order-tracking.php
@@ -0,0 +1,197 @@
+<?php include_once 'inc/header.php'  ?>
+
+
+<!-- Order Tracking Seation starts-->
+<div class="page-body">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="title-header option-title">
+                                    <h5>Order Tracking</h5>
+                                </div>
+                                <div class="row">
+                                    <div class="col-12 overflow-hidden">
+                                        <div class="order-left-image">
+                                            <div class="tracking-product-image">
+                                                <img src="assets/images/product/1.png" class="img-fluid w-100 blur-up lazyload" alt="">
+                                            </div>
+
+                                            <div class="order-image-contain">
+                                                <h4>Van Heusen Men's Solid Regular Fit Polo</h4>
+                                                <div class="tracker-number">
+                                                    <p>Order Number : <span>W765EWR8568871</span></p>
+                                                    <p>Brand : <span>Van Heusen</span></p>
+                                                    <p>Order Placed : <span>June 25, 2021</span></p>
+                                                </div>
+                                                <h5>Your items is on the way. Tracking information will be
+                                                    available within 24 hours.</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <ol class="progtrckr">
+                                        <li class="progtrckr-done">
+                                            <h5>Order Processing</h5>
+                                            <h6>05:43 AM</h6>
+                                        </li>
+                                        <li class="progtrckr-done">
+                                            <h5>Pre-Production</h5>
+                                            <h6>01:21 PM</h6>
+                                        </li>
+                                        <li class="progtrckr-done">
+                                            <h5>In Production</h5>
+                                            <h6>Processing</h6>
+                                        </li>
+                                        <li class="progtrckr-todo">
+                                            <h5>Shipped</h5>
+                                            <h6>Pending</h6>
+                                        </li>
+                                        <li class="progtrckr-todo">
+                                            <h5>Delivered</h5>
+                                            <h6>Pending</h6>
+                                        </li>
+                                    </ol>
+
+                                    <div class="col-12 overflow-visible">
+                                        <div class="tracker-table">
+                                            <div class="table-responsive">
+                                                <table class="table">
+                                                    <thead>
+                                                        <tr class="table-head">
+                                                            <th scope="col">Date</th>
+                                                            <th scope="col">Time</th>
+                                                            <th scope="col">Discription</th>
+                                                            <th scope="col">Location</th>
+                                                        </tr>
+                                                    </thead>
+
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>
+                                                                <h6>21/05/2021</h6>
+                                                            </td>
+                                                            <td>
+                                                                <h6>12:21 AM</h6>
+                                                            </td>
+                                                            <td>
+                                                                <p class="fw-bold">Shipped Info</p>
+                                                            </td>
+                                                            <td>
+                                                                <h6>3 SW. Summit St. Lithonia, GA 30038</h6>
+                                                            </td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td>
+                                                                <h6>15/04/2021</h6>
+                                                            </td>
+                                                            <td>
+                                                                <h6>01:00 PM</h6>
+                                                            </td>
+                                                            <td>
+                                                                <p class="fw-bold">Shipped</p>
+                                                            </td>
+                                                            <td>
+                                                                <h6>70 Rockwell Lane Falls Church, VA 22041
+                                                                </h6>
+                                                            </td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td>
+                                                                <h6>04/05/2021</h6>
+                                                            </td>
+                                                            <td>
+                                                                <h6>03:58 AM</h6>
+                                                            </td>
+                                                            <td>
+                                                                <p class="fw-bold">Shipped Info Received</p>
+                                                            </td>
+                                                            <td>
+                                                                <h6>13 Durham St. The Villages, FL 32162
+                                                                </h6>
+                                                            </td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td>
+                                                                <h6>30/04/2021</h6>
+                                                            </td>
+                                                            <td>
+                                                                <h6>06:26 PM</h6>
+                                                            </td>
+                                                            <td>
+                                                                <p class="fw-bold">Origin Scan</p>
+                                                            </td>
+                                                            <td>
+                                                                <h6>38 Saxon Lane Mobile, AL 36605</h6>
+                                                            </td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td>
+                                                                <h6>02/02/2021</h6>
+                                                            </td>
+                                                            <td>
+                                                                <h6>03:45 PM</h6>
+                                                            </td>
+                                                            <td>
+                                                                <p class="fw-bold">Shipped Info Received</p>
+                                                            </td>
+                                                            <td>
+                                                                <h6>3 Willow Street Chillicothe, OH 45601
+                                                                </h6>
+                                                            </td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td>
+                                                                <h6>14/01/2021</h6>
+                                                            </td>
+                                                            <td>
+                                                                <h6>12:21 AM</h6>
+                                                            </td>
+                                                            <td>
+                                                                <p class="fw-bold">Shipped</p>
+                                                            </td>
+                                                            <td>
+                                                                <h6>35 Brickyard Rd. Marshalltown, IA 50158
+                                                                </h6>
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer text-end border-0 pb-0 d-flex justify-content-end">
+                                <button class="btn btn-primary me-3">Submit</button>
+                                <button class="btn btn-outline">Cancel</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Container-fluid Ends-->
+
+    <!-- footer start-->
+    <div class="container-fluid">
+        <footer class="footer">
+            <div class="row">
+                <div class="col-md-12 footer-copyright text-center">
+                    <p class="mb-0">Copyright 2022 © Fastkart theme by pixelstrap</p>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!-- footer end-->
+</div>
+<?php include_once 'inc/footer.php'  ?>
